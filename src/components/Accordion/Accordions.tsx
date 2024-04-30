626b1984063a10d528b901375a15c71c58ea06ad
Mehar-Ali-123 <alihussniian0306@gmail.com>
2024-05-01 01:48:52 +0500

    check dashboard

--- src/components/Accordion/Accordions.tsx
+++ src/components/Accordion/Accordions.tsx
@@ -1,58 +1,70 @@
-import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
-import Accordion from 'react-bootstrap/Accordion';
-import "./__accordion.scss"
-function BasicExample() {
+import React, { useState } from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import Accordion from "react-bootstrap/Accordion";
+import "./__accordion.scss";
+function BasicExample({
+  headingPrimary,
+  subtitlePrimary,
+  headingSecondary,
+  subtitleSecondary,
+}) {
+  const [isAccordionOpen, setIsAccordionOpen] = useState(false);
 
+  const toggleAccordion = () => {
+    setIsAccordionOpen((prevState) => !prevState);
+  };
   return (
-    <Container fluid className='p-0'>
-      <Accordion className='mb-3'>
-        <Accordion.Item eventKey="0" className='accordion-bar'>
+    <Container fluid className="p-0 ">
+      <Accordion className="mb-3 ">
+        <Accordion.Item eventKey="0" className="accordion-bar"   onClick={toggleAccordion}>
           <Row>
-            <Accordion.Header className='w-100'>
+            <Accordion.Header className="w-100 pe-4 ">
               <Col xs={5}>
-                <h4 className='mb-1 accordion-title'> Delivery vs Collection</h4>
-                <p className='mb-0 accordion-subtitle'>Total Orders:<span className='order-value'>3.4k</span></p>
+                <h5 className="mb-1 accordion-title">{headingPrimary}</h5> 
+                  {subtitlePrimary}  
               </Col>
               <Col xs={5}>
-                <h4 className='mb-1 accordion-title'>Daily Order Completion</h4>
-                <p className='mb-0 accordion-subtitle'>Total Orders:<span className='order-value'>250</span></p>
+                <h5 className="mb-1 accordion-title">{headingSecondary}</h5>
+                {subtitleSecondary}
               </Col>
+              <Col xs={2} className="text-end">
+              <span 
+                className="accordion-showtitle"
+              >
+                {isAccordionOpen ? "Hide Details" : "Show Details"}
+              </span>
+            </Col>
             </Accordion.Header>
           </Row>
           <Accordion.Body>
             <Row>
               <Col xs={5}>
                 <Row>
-                  <Col xs={5} className='d-flex flex-column'>
-                    <div className='d-flex justify-content-between align-items-center mb-3'>
-                      <div className='d-flex align-items-center'>
-                        <span className='color-indicator'></span>
-                        <span className='indicator-next-title'>Delivery</span>
+                  <Col xs={5} className="d-flex flex-column">
+                    <div className="d-flex justify-content-between align-items-center mb-3">
+                      <div className="d-flex align-items-center">
+                        <span className="color-indicator"></span>
+                        <span className="indicator-next-title">Delivery</span>
                       </div>
-                      <p className='mb-0 order-qunatity'>74 Orders</p>
+                      <p className="mb-0 order-qunatity">74 Orders</p>
                     </div>
-                    <div className='d-flex justify-content-between align-items-center mb-3'>
-                      <div className='d-flex align-items-center'>
-                        <span className='color-indicator'></span>
-                        <span className='indicator-next-title'>Collection</span>
+                    <div className="d-flex justify-content-between align-items-center mb-3">
+                      <div className="d-flex align-items-center">
+                        <span className="color-indicator"></span>
+                        <span className="indicator-next-title">Collection</span>
                       </div>
-                      <p className='mb-0 order-qunatity'>56 Orders</p>
+                      <p className="mb-0 order-qunatity">56 Orders</p>
                     </div>
                   </Col>
-                  <Col xs={7}>
-                    
-                  </Col>
+                  <Col xs={7}></Col>
                 </Row>
               </Col>
-              <Col xs={5}>
-                Accordion Item #1
-              </Col>
+              <Col xs={5}>Accordion Item #1</Col>
             </Row>
           </Accordion.Body>
         </Accordion.Item>
       </Accordion>
-    </Container >
+    </Container>
   );
 }
 
--- src/components/OrderDetails/OrderDetails.tsx
+++ src/components/OrderDetails/OrderDetails.tsx
@@ -1,17 +1,113 @@
-import React from 'react'
-import Accordions from '../Accordion/Accordions'
-import { Col, Container, Row } from 'react-bootstrap'
-
+import React from "react";
+import Accordions from "../Accordion/Accordions";
+import { Col, Container, Row } from "react-bootstrap";
+import "../Accordion/__accordion.scss"
 const OrderDetails = () => {
-    return (
-        <Container fluid className='mt-3 p-0'>
-            <Row>
-                <Col>
-                    <Accordions />
-                </Col>
-            </Row>
-        </Container>
-    )
-}
+  return (
+    <Container fluid className="mt-3 p-0">
+      <Row>
+        <Col xs={12}>
+          <Accordions
+            headingPrimary="Delivery vs Collection vs Depot"
+            headingSecondary="Daily Order Completion"
+            subtitlePrimary={
+              <p className="mb-0 accordion-subtitle">
+                Total Orders:
+                <span className="order-value">3.4k</span>
+              </p>
+            }
+            subtitleSecondary={
+              <p className="mb-0 accordion-subtitle">
+                Total Orders:
+                <span className="order-value text-small">250</span>
+              </p>
+            }
+          />
+        </Col>
+        <Col xs={12}>
+          <Accordions
+            headingPrimary="Failed Delivery Reasons"
+            headingSecondary=""
+            subtitlePrimary={
+              <p className="mb-0 accordion-subtitle">
+                Total Failed Deliveries:
+                <span className="order-value">3700</span>
+              </p>
+            }
+            subtitleSecondary={
+              <p className="mb-0 accordion-subtitle"></p>
+            }
+          />
+        </Col>
+        <Col xs={12}>
+          <Accordions
+            headingPrimary="Recuring ROute Frequency Count"
+            headingSecondary="Orders With Missing Items"
+            subtitlePrimary={
+              <p className="mb-0 accordion-subtitle">
+                Total Routs:
+                <span className="order-value">170</span>
+              </p>
+            }
+            subtitleSecondary={
+              <p className="mb-0 accordion-subtitle">
+                Total Deliveries:
+                <span className="order-value">3700</span>
+              </p>
+            }
+          />
+        </Col>
+        <Col xs={12}>
+          <Accordions
+            headingPrimary="Top 5 Customers With Missing Items"
+            headingSecondary=""
+            subtitlePrimary={
+              <p className="mb-0 accordion-subtitle">
+                Total Missing Items:
+                <span className="order-value">3700</span>
+              </p>
+            }
+            subtitleSecondary={
+              <p className="mb-0 accordion-subtitle"></p>
+            }
+          />
+        </Col>
+        <Col xs={12}>
+          <Accordions
+            headingPrimary="Total 5 Reasons For Latness"
+            headingSecondary=""
+            subtitlePrimary={
+              <p className="mb-0 accordion-subtitle">
+                Total Drivers & Vehicles:
+                <span className="order-value">3700</span>
+              </p>
+            }
+            subtitleSecondary={
+              <p className="mb-0 accordion-subtitle"></p>
+            }
+          />
+        </Col>
+        <Col xs={12}>
+          <Accordions
+            headingPrimary="Vehicle Registration With Major & Minor Counts"
+            headingSecondary="Count Of Deliveries By Hour"
+            subtitlePrimary={
+              <p className="mb-0 accordion-subtitle">
+                Total Driver & Vehicles:
+                <span className="order-value">170</span>
+              </p>
+            }
+            subtitleSecondary={
+              <p className="mb-0 accordion-subtitle">
+                Total Deliveries:
+                <span className="order-value">3700</span>
+              </p>
+            }
+          />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
-export default OrderDetails
+export default OrderDetails;
